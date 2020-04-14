cc080e10f2cb8d9eb23021f8d942371d1bbec61c
martinyounghoonkim <hoons0131@gmail.com>
2020-04-14 21:05:21 +0900

    initial springboot project

---
+++ src/test/java/com/backend/application28301/Application28301ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.application28301;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Application28301ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
